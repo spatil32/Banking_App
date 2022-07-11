ead829a4652d7755eeea3f04db9c41961a508ee4
Shreyas Patil <shpatil@.com>
2022-07-10 23:37:08 -0700

    add functinoality for transfer money, request loan, close account and sort functionality

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const account4 = {
 const accounts = [account1, account2, account3, account4];
 
 // Elements
-const wrongPin = document.getElementById('wrong-pin');
+const loginError = document.getElementById('wrong-pin');
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
 const labelBalance = document.querySelector('.balance__value');
@@ -42,6 +42,7 @@ const labelSumIn = document.querySelector('.summary__value--in');
 const labelSumOut = document.querySelector('.summary__value--out');
 const labelSumInterest = document.querySelector('.summary__value--interest');
 const labelTimer = document.querySelector('.timer');
+const invalidLoan = document.getElementById('invalid-loan');
 
 const containerApp = document.querySelector('.app');
 const containerMovements = document.querySelector('.movements');
@@ -59,6 +60,7 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+const notEnoughBalance = document.getElementById('not_enough_balance');
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -66,9 +68,10 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const displayMovements = (movements) => {
+const displayMovements = (movements, sort = false) => {
+	const movementsCpy = sort ? movements.slice().sort((a, b) => a - b) : movements;
 	containerMovements.innerHTML = '';
-	movements.forEach((movement, i) => {
+	movementsCpy.forEach((movement, i) => {
 		const transaction = movement < 0 ? 'withdrawal' : 'deposit';
 		const html = `
 			<div class="movements__row">
@@ -87,9 +90,9 @@ const createUsernames = (accounts) => {
 	});
 };
 
-const calcPrintBalance = (movements) => {
-	const currBalance = movements.reduce((acc, curr) => acc + curr, 0);
-	labelBalance.textContent = `${currBalance}€`
+const calcPrintBalance = (account) => {
+	account.balance = account.movements.reduce((acc, curr) => acc + curr, 0);
+	labelBalance.textContent = `${account.balance}€`
 }
 
 const displaySummary = (account) => {
@@ -118,6 +121,12 @@ const displaySummary = (account) => {
 	labelSumInterest.textContent = `${Math.abs(interest)}€`;
 }
 
+const updateUI = (account) => {
+	displayMovements(account.movements);
+	calcPrintBalance(account);
+	displaySummary(account);
+}
+
 let currentAccount;
 
 btnLogin.addEventListener('click', (event) => {
@@ -125,20 +134,81 @@ btnLogin.addEventListener('click', (event) => {
 	// find current logged in user
 	currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value)
 	if(currentAccount?.pin === +inputLoginPin.value) {
-		wrongPin.classList.add('hide');
+		loginError.classList.add('hide');
 		// if user exists and pin matches, show the account info
 		labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(' ')[0]}`
 		displayMovements(currentAccount.movements);
-		calcPrintBalance(currentAccount.movements);
+		calcPrintBalance(currentAccount);
 		displaySummary(currentAccount);
 		containerApp.style.opacity = 100;
 
 		inputLoginUsername.value = inputLoginPin.value = '';
 		inputLoginPin.blur();
-	} else {
+	} 
+	else if(!currentAccount) {
+		loginError.textContent = 'No User found! Try again...';
+		inputLoginUsername.value = inputLoginPin.value = '';
+
+		loginError.classList.remove('hide');
+	}
+	else {
+		loginError.textContent = 'Wrong Password! Please try again...'
 		inputLoginPin.value = '';
-		wrongPin.classList.remove('hide');
+		loginError.classList.remove('hide');
 	}
 });
 
+btnTransfer.addEventListener('click', (event) => {
+	event.preventDefault();
+	const amount = Number(inputTransferAmount.value);
+	const accountToTransfer = accounts.find(account => account.username === inputTransferTo.value);
+	
+	if(amount > 0 && accountToTransfer && currentAccount.balance >= amount && accountToTransfer?.username !== currentAccount.username) {
+		notEnoughBalance.classList.add('hide');
+		currentAccount.movements.push(-amount);
+		accountToTransfer.movements.push(amount);
+
+		inputTransferAmount.value = inputTransferTo.value = '';
+		updateUI(currentAccount);
+	} else {
+		inputTransferAmount.value = '';
+		notEnoughBalance.classList.remove('hide');
+	}
+});
+
+btnClose.addEventListener('click', (event) => {
+	event.preventDefault();
+	const accountToClose = inputCloseUsername.value;
+	const accountPinToClose = Number(inputClosePin.value);
+	
+	if(accountToClose === currentAccount.username && accountPinToClose === currentAccount.pin) {
+		const accountIndex = accounts.findIndex(account => account.username === currentAccount.username && account.pin === currentAccount.pin);
+		accounts.splice(accountIndex, 1);
+		containerApp.style.opacity = 0;
+	}
+
+	inputCloseUsername.value = inputClosePin.value = '';
+});
+
+btnLoan.addEventListener('click', (event) => {
+	event.preventDefault();
+	const amount = Number(inputLoanAmount.value);
+	if(amount > 0 && currentAccount.movements.some(movement => movement >= amount * 0.1)) {
+		invalidLoan.classList.add('hide');
+
+		currentAccount.movements.push(amount);
+		updateUI(currentAccount);
+	} else {
+		invalidLoan.textContent = `Cannot take loan of ${inputLoanAmount}! Try different amount...`;
+		invalidLoan.classList.remove('hide');
+	}
+	inputLoanAmount.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', (event) => {
+	event.preventDefault();
+	displayMovements(currentAccount.movements, !sorted);
+	sorted = !sorted;
+})
 createUsernames(accounts);
